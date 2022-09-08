94dad12eb3880daedfc28dafe6307f22135f08cd
Nadezhda <nbykova79@gmail.com>
2022-09-08 19:49:05 +0300

    задача о сумме целых чисел от 1 до N

---
+++ TASK02.py
@@ -0,0 +1,10 @@
+# Требуется посчитать сумму целых чисел, расположенных между числами 1 и N включительно.
+
+N=int(input('Введите число: '))
+
+def Summery(N):
+    sum=0
+    for i in range(N+1):
+        sum+=i
+    return sum
+print(Summery(N))
