317a301089d69aa6aac4e5eb759696338d46740e
Nadezhda <nbykova79@gmail.com>
2022-09-09 19:41:02 +0300

    задача про сбор ягод

---
+++ TASK05.py
@@ -0,0 +1,10 @@
+n = int(input('Введите кол-во кустов: '))
+arr = list()
+for i in range(n):
+    arr.append(int(input('Введите кол-во ягод ')))
+
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(max(arr_count))
